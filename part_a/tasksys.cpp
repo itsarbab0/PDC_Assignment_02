9311ba3731388172ac457dcf5301cce534f5b84c
itsarbab0 <kareemarbab00000@gmail.com>
2025-03-16 05:43:30 -0400

    Updation in tastsys.cpp

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -81,7 +81,7 @@ void TaskSystemParallelSpawn::run(IRunnable* runnable, int num_total_tasks) {
     //     runnable->runTask(i, num_total_tasks);
     // }
 
-    ///// Now I am implementing my Logic for  Task Parallel with Spawn
+    // ///// Now I am implementing my Logic for  Task Parallel with Spawn
     vector<thread> threadsarr;     // Making array for threads
 
     // Now Spawn new thread for each task
@@ -99,58 +99,7 @@ void TaskSystemParallelSpawn::run(IRunnable* runnable, int num_total_tasks) {
     }
 
 
-    ////   Second approach
-
-    // vector<thread> arr;
-    // queue<int> tq;
-    // mutex mtxlock;
-    // condition_variable vari;
-    // bool taskDone = false;
-    // int num_threads = 16;
-    // /// Filling queue with tasks
-    // for(int i = 0; i < num_total_tasks; i++)
-    // {
-    //     tq.push(i);
-    // }
-
-    // for(int i = 0; i < num_threads; i++)
-    // {
-    //     arr.push_back(thread([&tq, &mtxlock, &vari, &taskDone, runnable, num_total_tasks]() 
-    //     {
-    //         while (true) 
-    //         {
-    //             unique_lock<mutex> lock(mtxlock);
-                
-                
-    //             vari.wait(lock, [&tq, &taskDone]() { return !tq.empty() || taskDone; });
-
-    //             if (taskDone && tq.empty())
-    //             { 
-    //                 break;
-    //             }
-
-    //             int taskid = tq.front();
-    //             tq.pop();
-    //             lock.unlock();
-
-                
-    //             runnable->runTask(taskid, num_total_tasks);
-    //         }
-    //     }));
-    // }
-    // {
-    //     lock_guard<mutex>lock(mtxlock);
-    //     vari.notify_all();
-    // }
-    // for(auto& t : arr)
-    // {
-    //     t.join();
-    // }
-    // {
-    //     lock_guard<mutex> lock(mtxlock);
-    //     taskDone = true;
-    // }
-    // vari.notify_all();
+    
 
 }
 
@@ -185,106 +134,141 @@ TaskSystemParallelThreadPoolSpinning::TaskSystemParallelThreadPoolSpinning(int n
 }
 
 TaskSystemParallelThreadPoolSpinning::~TaskSystemParallelThreadPoolSpinning() {}
-
-void TaskSystemParallelThreadPoolSpinning::run(IRunnable* runnable, int num_total_tasks) {
-
-
-    //
-    // TODO: CS149 students will modify the implementation of this
-    // method in Part A.  The implementation provided below runs all
-    // tasks sequentially on the calling thread.
-    //
-
-    // for (int i = 0; i < num_total_tasks; i++) {
-    //     runnable->runTask(i, num_total_tasks);
-    // }
-
-    ///// Now here is my Logoc for TaskSysParalell Thread Pool Spi
-    // int num_threads = 16;
-    // vector<thread> threadsarr;
-    // mutex mtxlock;
-    // bool taskDone = false;
-    // for(int i = 0; i < num_threads; i++)
-    // {
-    //     threadsarr.push_back(thread([&mtxlock, &taskDone, this, runnable, num_total_tasks](){
-    //         while(true)
-    //         {
-    //             lock_guard<mutex> lock(mtxlock);
-    //             if(taskDone)
-    //             {
-    //                 break;
-    //             }
-
-    //             for(int taskid = 0; taskid < num_total_tasks; taskid++)
-    //             {
-    //                 runnable->runTask(taskid, num_total_tasks);
-    //             }
-    //         }
-    //     }));
-    // }
-
-    // {
-    //     lock_guard<mutex> lock(mtxlock);
-    //     taskDone = true;
-    // }
-
-    // for(auto& t : threadsarr)
-    // {
-    //     t.join();
-    // }
-
-
-
-    ////  Second Approach
-    vector<thread> arr;
-    queue<int> tq;
-    mutex mtxlock; 
-    condition_variable vari;
+void TaskSystemParallelThreadPoolSpinning::run(IRunnable* runnable, int num_total_tasks) 
+{
+    vector<thread> threadsarr;
+    mutex mtxlock;
     bool taskDone = false;
     int num_threads = 16;
-    for (int i = 0; i < num_total_tasks; i++)
+   
+    for (int i = 0; i < num_threads; i++) 
     {
-        tq.push(i);
-    }
-
-    for(int i = 0; i < num_threads; i++)
-    {
-        arr.push_back(thread([&tq, &mtxlock, &vari, &taskDone, runnable, num_total_tasks]()
+        threadsarr.push_back(thread([&mtxlock, &taskDone, this, runnable, num_total_tasks]() 
         {
+           
+            lock_guard<mutex> lock(mtxlock);
+
             
-            while(true)
+            for (int taskid = 0; taskid < num_total_tasks; taskid++) 
             {
-                unique_lock<mutex> lock(mtxlock);
-                vari.wait(lock, [&tq, &taskDone]() { return !tq.empty() || taskDone;});
-
-                if(taskDone && tq.empty())
-                {
-                    break;
-                }
-
-                int tasid = tq.front();
-                tq.pop();
-                lock.unlock();
-                runnable->runTask(tasid, num_total_tasks);
+                runnable->runTask(taskid, num_total_tasks);
             }
         }));
     }
 
+   
     {
         lock_guard<mutex> lock(mtxlock);
-        vari.notify_all();
+        taskDone = true;  
     }
-    for(auto& t : arr)
-    {
-        t.join();
-    }
-    {
-        lock_guard<mutex> lock(mtxlock);
-        taskDone = true;
-    }
-    vari.notify_all();
 
+    for (auto& t : threadsarr) 
+    {
+
+        t.join(); 
+    }
 }
+
+
+// void TaskSystemParallelThreadPoolSpinning::run(IRunnable* runnable, int num_total_tasks) {
+
+
+//     //
+//     // TODO: CS149 students will modify the implementation of this
+//     // method in Part A.  The implementation provided below runs all
+//     // tasks sequentially on the calling thread.
+//     //
+
+//     // for (int i = 0; i < num_total_tasks; i++) {
+//     //     runnable->runTask(i, num_total_tasks);
+//     // }
+
+//     ///// Now here is my Logoc for TaskSysParalell Thread Pool Spi
+//     // int num_threads = 16;
+//     // vector<thread> threadsarr;
+//     // mutex mtxlock;
+//     // bool taskDone = false;
+//     // for(int i = 0; i < num_threads; i++)
+//     // {
+//     //     threadsarr.push_back(thread([&mtxlock, &taskDone, this, runnable, num_total_tasks](){
+//     //         while(true)
+//     //         {
+//     //             lock_guard<mutex> lock(mtxlock);
+//     //             if(taskDone)
+//     //             {
+//     //                 break;
+//     //             }
+
+//     //             for(int taskid = 0; taskid < num_total_tasks; taskid++)
+//     //             {
+//     //                 runnable->runTask(taskid, num_total_tasks);
+//     //             }
+//     //         }
+//     //     }));
+//     // }
+
+//     // {
+//     //     lock_guard<mutex> lock(mtxlock);
+//     //     taskDone = true;
+//     // }
+
+//     // for(auto& t : threadsarr)
+//     // {
+//     //     t.join();
+//     // }
+
+
+
+//     ////  Second Approach
+//     vector<thread> arr;
+//     queue<int> tq;
+//     mutex mtxlock; 
+//     condition_variable vari;
+//     bool taskDone = false;
+//     int num_threads = 16;
+//     for (int i = 0; i < num_total_tasks; i++)
+//     {
+//         tq.push(i);
+//     }
+
+//     for(int i = 0; i < num_threads; i++)
+//     {
+//         arr.push_back(thread([&tq, &mtxlock, &vari, &taskDone, runnable, num_total_tasks]()
+//         {
+            
+//             while(true)
+//             {
+//                 unique_lock<mutex> lock(mtxlock);
+//                 vari.wait(lock, [&tq, &taskDone]() { return !tq.empty() || taskDone;});
+
+//                 if(taskDone && tq.empty())
+//                 {
+//                     break;
+//                 }
+
+//                 int tasid = tq.front();
+//                 tq.pop();
+//                 lock.unlock();
+//                 runnable->runTask(tasid, num_total_tasks);
+//             }
+//         }));
+//     }
+
+//     {
+//         lock_guard<mutex> lock(mtxlock);
+//         vari.notify_all();
+//     }
+//     for(auto& t : arr)
+//     {
+//         t.join();
+//     }
+//     {
+//         lock_guard<mutex> lock(mtxlock);
+//         taskDone = true;
+//     }
+//     vari.notify_all();
+
+// }
 
 TaskID TaskSystemParallelThreadPoolSpinning::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                                               const vector<TaskID>& deps) {
@@ -324,95 +308,135 @@ TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
     // (requiring changes to tasksys.h).
     //
 }
-
-void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
-
-
-    //
-    // TODO: CS149 students will modify the implementation of this
-    // method in Parts A and B.  The implementation provided below runs all
-    // tasks sequentially on the calling thread.
-    //
-
-    // for (int i = 0; i < num_total_tasks; i++) {
-    //     runnable->runTask(i, num_total_tasks);
-    // }
-
-    // //// Now here I am Implenting  my Logic for TaskSysParalell Thread Pool Sleep
-    // int num_threads = 16;
-    // vector<thread> threadsarr;
-    // mutex mtxlock;
-    // condition_variable vari;
-    // bool taskDone = false;
-    // for(int i = 0; i < num_threads; i++)
-    // {
-    //     threadsarr.push_back(thread([&vari, &mtxlock, &taskDone, this, runnable, num_total_tasks]()
-    //     {
-    //         unique_lock<mutex> lock(mtxlock);
-    //         while(!taskDone)
-    //         {
-    //             vari.wait(lock);
-    //             for(int taskid = 0; taskid < num_total_tasks; taskid++)
-    //             {
-    //                 runnable->runTask(taskid, num_total_tasks);
-    //             }
-    //         }
-    //     }));
-    // }
-
-    // {
-    //     lock_guard<mutex> lock(mtxlock);
-    //     taskDone = true;
-    // }
-    // vari.notify_all();
-
-    // for(auto& t : threadsarr)
-    // {
-    //     t.join();
-    // }
-
-
-
-    vector<thread> arr;
-    mutex mtxlock;
-    condition_variable vari;
-    bool taskDone = false;
+void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) 
+{
+    vector<thread> threadsarr;
+    mutex taskMutex;
+    condition_variable cv;
+    bool tasksDone = false;
     int num_threads = 16;
-
     
+
     for (int i = 0; i < num_threads; i++) 
     {
-        arr.push_back(thread([&mtxlock, &vari, &taskDone, runnable, num_total_tasks]() 
+        threadsarr.push_back(thread([&taskMutex, &cv, &tasksDone, runnable, num_total_tasks]() 
         {
-            unique_lock<mutex> lock(mtxlock);
+            unique_lock<mutex> lock(taskMutex);
 
-            
-            while (!taskDone) 
+            while (!tasksDone) 
             {
-                vari.wait(lock);  
-                for (int taskid = 0; taskid < num_total_tasks; taskid++)
+                cv.wait(lock);  
+                for (int taskid = 0; taskid < num_total_tasks; taskid++) 
                 {
-                    runnable->runTask(taskid, num_total_tasks);
+                    runnable->runTask(taskid, num_total_tasks);  
                 }
             }
         }));
     }
 
-
+    
     {
-        lock_guard<mutex> lock(mtxlock);
-        taskDone = true;  
+        lock_guard<mutex> lock(taskMutex);
+        tasksDone = true;  
     }
 
-    vari.notify_all();  
+    cv.notify_all();  
 
-    for (auto& t : arr) 
+    
+    for (auto& t : threadsarr) 
     {
-        t.join();
+        t.join(); 
     }
-
-
 }
+
+//  void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
+
+
+//     //
+//     // TODO: CS149 students will modify the implementation of this
+//     // method in Parts A and B.  The implementation provided below runs all
+//     // tasks sequentially on the calling thread.
+//     //
+
+//     // for (int i = 0; i < num_total_tasks; i++) {
+//     //     runnable->runTask(i, num_total_tasks);
+//     // }
+
+//     // //// Now here I am Implenting  my Logic for TaskSysParalell Thread Pool Sleep
+//     // int num_threads = 16;
+//     // vector<thread> threadsarr;
+//     // mutex mtxlock;
+//     // condition_variable vari;
+//     // bool taskDone = false;
+//     // for(int i = 0; i < num_threads; i++)
+//     // {
+//     //     threadsarr.push_back(thread([&vari, &mtxlock, &taskDone, this, runnable, num_total_tasks]()
+//     //     {
+//     //         unique_lock<mutex> lock(mtxlock);
+//     //         while(!taskDone)
+//     //         {
+//     //             vari.wait(lock);
+//     //             for(int taskid = 0; taskid < num_total_tasks; taskid++)
+//     //             {
+//     //                 runnable->runTask(taskid, num_total_tasks);
+//     //             }
+//     //         }
+//     //     }));
+//     // }
+
+//     // {
+//     //     lock_guard<mutex> lock(mtxlock);
+//     //     taskDone = true;
+//     // }
+//     // vari.notify_all();
+
+//     // for(auto& t : threadsarr)
+//     // {
+//     //     t.join();
+//     // }
+
+
+
+//     vector<thread> arr;
+//     mutex mtxlock;
+//     condition_variable vari;
+//     bool taskDone = false;
+//     int num_threads = 16;
+
+    
+//     for (int i = 0; i < num_threads; i++) 
+//     {
+//         arr.push_back(thread([&mtxlock, &vari, &taskDone, runnable, num_total_tasks]() 
+//         {
+//             unique_lock<mutex> lock(mtxlock);
+
+            
+//             while (!taskDone) 
+//             {
+//                 vari.wait(lock);  
+//                 for (int taskid = 0; taskid < num_total_tasks; taskid++)
+//                 {
+//                     runnable->runTask(taskid, num_total_tasks);
+//                 }
+//             }
+//         }));
+//     }
+
+
+//     {
+//         lock_guard<mutex> lock(mtxlock);
+//         taskDone = true;  
+//     }
+
+//     vari.notify_all();  
+
+//     for (auto& t : arr) 
+//     {
+//         t.join();
+//     }
+
+
+// }
 
 TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                                     const vector<TaskID>& deps) {
